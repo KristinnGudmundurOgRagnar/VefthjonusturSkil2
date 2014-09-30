7d9436d3393ebc8dc38a44302b1723e439ce2484
gudmundurste12 <gudmundurste12@ru.is>
2014-09-30 16:49:13 +0000

    Added ProjectController

---
+++ CoursesAPI/Controllers/ProjectController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CoursesAPI.Services.Services;
+using CoursesAPI.Models;
+using CoursesAPI.Services.Models;
+using CoursesAPI.Services.DataAccess;
+
+namespace CoursesAPI.Controllers
+{
+	[RoutePrefix("api/courses/{courseInstanceID}")]
+    public class ProjectController : ApiController
+    {
+		private readonly CoursesServiceProvider _service;
+
+		public ProjectController()
+		{
+			_service = new CoursesServiceProvider(new UnitOfWork<AppDataContext>());
+		}
+
+		[Route("project")]
+		public List<int> GetProjects(int courseInstanceID)
+		{
+
+			return new List<int> { 1,2,3,4,5,6,7};
+		}
+    }
+}

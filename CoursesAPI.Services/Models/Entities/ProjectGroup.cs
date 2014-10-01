40a82558c1ca32034b5d784b9e7644d50d278c1a
gudmundurste12 <gudmundurste12@ru.is>
2014-10-01 10:42:13 +0000

    Added ProjectGroup.cs

---
+++ CoursesAPI.Services/Models/Entities/ProjectGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoursesAPI.Services.Models.Entities
+{
+	[Table("ProjectGroup")]
+	class ProjectGroup
+	{
+		/// <summary>
+		/// A database generated Id of the project group
+		/// </summary>
+		public int ID { get; set; }
+
+		/// <summary>
+		/// The name of the project group
+		/// </summary>
+		public String Name { get; set; }
+
+		/// <summary>
+		/// The number of projects in this group that will be used to calculate a final grade
+		/// </summary>
+		public int GradedProjectsCount { get; set; }
+	}
+}

0d7f278e74c7287892daa5d5fd73d22a1066d83f
gudmundurste12 <gudmundurste12@ru.is>
2014-10-02 14:47:29 +0000

    Added an entity class for FinalGradeCompositions

---
+++ CoursesAPI.Services/Models/Entities/FinalGradeComposition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoursesAPI.Services.Models.Entities
+{
+	[Table("FinalGradeCompositions")]
+	public class FinalGradeComposition
+	{
+		/// <summary>
+		/// The database-generated Id of the record
+		/// </summary>
+		public int ID { get; set; }
+		/// <summary>
+		/// The Id of the courseInstance
+		/// </summary>
+		public int CourseInstanceId { get; set; }
+		/// <summary>
+		/// The Id of the project
+		/// </summary>
+		public int ProjectId { get; set; }
+	}
+}

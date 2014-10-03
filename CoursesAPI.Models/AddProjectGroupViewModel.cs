2a0a5f4b13bb66bf4bbd3a43b09f2218edfa3b4b
kristinnv12 <kristinnv12@ru.is>
2014-10-03 11:50:27 +0000

    projectgroup

---
+++ CoursesAPI.Models/AddProjectGroupViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursesAPI.Models
+{
+    public class AddProjectGroupViewModel
+    {
+        //Name of ProjectGroup
+        public String Name { get; set; }
+        //number of top projects to be evaluated
+        public int? GradedProjectsCount { get; set; }
+
+    }
+}
